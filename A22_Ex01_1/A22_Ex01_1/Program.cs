982244b88fb4604369c9cdebe3a49b9ef1e7640c
PUBLICIS\omer_p <omer_p@mintapp.co.il>
2021-11-09 17:42:15 +0200

    init

---
+++ A22_Ex01_1/A22_Ex01_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace A22_Ex01_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Input();
+            Input();
+            Input();
+            Input();
+        }
+
+        static int Input()
+        {
+            return 0;
+        }
+    }
+}
