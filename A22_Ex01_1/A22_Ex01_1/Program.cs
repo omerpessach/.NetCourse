b9f2c6f00cb71d6b86657f8fe5b763c51cbc8d54
PUBLICIS\omer_p <omer_p@mintapp.co.il>
2021-11-11 12:52:21 +0200

    Update Program.cs

--- A22_Ex01_1/A22_Ex01_1/Program.cs
+++ A22_Ex01_1/A22_Ex01_1/Program.cs
@@ -50,7 +50,7 @@ namespace A22_Ex01_1
                 countOfNumbersThatPowOfTwo++;
             }
 
-            int convertedNumber = Convert.ToInt32(userInput, 2);
+            int convertedNumber = ConvertStringToDecimelNumber(userInput);
 
             if (maxNumber < convertedNumber)
             {
@@ -63,29 +63,15 @@ namespace A22_Ex01_1
             }
 
             return convertedNumber;
-        static int Input()
-        {
-            string inputRequest = "Please enter number in binary format";
-            Console.WriteLine("Please enter number in binary format");
-            string userInput = Console.ReadLine();
-
-            while (userInput.Length == 0 && (!userInput.Contains("0") || !userInput.Contains("1")))
-            {
-                Console.WriteLine($"The correct input is wrong, please try again\n {inputRequest}");
-                userInput = Console.ReadLine();
-            }
-
-            int convertedNumber = Convert.ToInt32(userInput, 2);
-            return convertedNumber;
         }
 
         static int ConvertStringToDecimelNumber(string i_str2Dec)
         {
             double convertedDecNumber = 0;
 
-            for(int i = 0; i< i_str2Dec.Length; i++)
+            for (int i = 0; i < i_str2Dec.Length; i++)
             {
-                if(i_str2Dec[i] ==  '1')
+                if (i_str2Dec[i] == '1')
                 {
                     convertedDecNumber += Math.Pow(2, 7 - i - 1);
                 }
