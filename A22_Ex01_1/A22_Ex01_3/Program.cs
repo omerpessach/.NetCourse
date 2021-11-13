486c10393ff3db7c846971cc2a042557ad063bbc
PUBLICIS\omer_p <omer_p@mintapp.co.il>
2021-11-13 22:23:33 +0200

    some fixes

---
+++ A22_Ex01_1/A22_Ex01_3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Ex2 = A22_Ex01_2;
+
+namespace A22_Ex01_3
+{
+    class Program
+    {
+        static void Main()
+        {
+            int inputFromUserAsUInt;
+
+            inputFromUserAsUInt = GetInputFromUser();
+            Ex2.Program.HourGlass(inputFromUserAsUInt);
+        }
+
+        static int GetInputFromUser()
+        {
+            int userInputAsNumber;
+            string inputRequest = "Please enter the height of the hour Glass";
+            string userInput;
+
+            Console.WriteLine(inputRequest);
+            userInput = Console.ReadLine();
+            while (!int.TryParse(userInput, out userInputAsNumber) || userInputAsNumber < 0)
+            {
+                Console.WriteLine(string.Format("The input is wrong, please try again\n {0}", inputRequest));
+                userInput = Console.ReadLine();
+            }
+
+            return userInputAsNumber;
+        }
+    }
+}
