7d16d56bf5d4c413b5974702fe108860b6a03b8a
PUBLICIS\omer_p <omer_p@mintapp.co.il>
2021-12-16 16:19:38 +0200

    Init ex03

---
+++ A22 Ex03 Eden 315900019 Omer 318854387/A22 Ex03 Eden 315900019 Omer 318854387/Ex03.ConsoleUI/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex03.ConsoleUI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
